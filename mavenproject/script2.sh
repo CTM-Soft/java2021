3b241ea41a84501fe9d9c40813f71970534eb32e
root <root@ip-172-31-44-133.eu-west-2.compute.internal>
2022-01-20 05:15:09 +0000

    Mvnproj added

--- mavenproject/script2.sh
+++ mavenproject/script2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash/
 git clone https://github.com/CTM-Soft/java2021.git
-cd java2021/Mvn2021Proj-1/
+cd java2021/mavenproject/
 mvn clean install
 if [[ $? -ne 0 ]]; then
 echo "build failed"
